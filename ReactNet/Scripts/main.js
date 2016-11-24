bb5b194c69ec16f4109d641f88cb0cc31a6fa56e
Eros Niko Alvarez <iamerosniko@yahoo.com>
2016-11-24 16:36:03 -0500

    changes in ui

--- ReactNet/Scripts/main.js
+++ ReactNet/Scripts/main.js
@@ -9,15 +9,10 @@ var ProjectLists = React.createClass({
         return (
         
         <div className="Container">
-            <h1>Applications</h1>
-            <ul className="header">
-                <li>Application 1</li>
-                <li>Application 2</li>
-            </ul>
-            <div className="content">
-                this is from content
-            </div>
-        
+            <h1>Projects</h1>
+            Project name: <input type="text" /> 
+            Project Description: <input type="text" /> 
+
             <a>back</a>
         </div>
         )
@@ -25,30 +20,42 @@ var ProjectLists = React.createClass({
 });
 
 var Main = React.createClass({
-  render: function() {
-      if(this.props.nums == 1){
-          return (
+    render: function() {
+        if(this.props.nums == 1){
+            return (
         
-          <div className="Container">
-              <h1>Projects</h1>
-              <ul className="header">{
-                  this.props.projects.map(function(c){
-                      return (<li>{c.Name} </li>)
-                  })}
-
-              </ul>
-          </div>
-          )
+                <div className="Container">
+                    <h1>Projects</h1>
+                    <table>
+                        <tr>
+                            <th> Name </th>
+                            <th> Description </th>
+                            <th></th>
+                        </tr>
+               
+                        {this.props.projects.map(function(c){
+                            return ( 
+                                <tr> 
+                                        <td>{c.Name}</td>
+                                    <td>{c.Desc}</td>
+                                    <td> <a href="#" >
+                                    <span className="glyphicon glyphicon-remove delete-obj"></span></a></td>
+                                </tr>
+                                )
+                        })}
+               
+                    </table>
+        
+                </div>
+            )
       }
       else{
           return(
-              <ProjectLists number={this.props.nums}/>
+              <ProjectLists number={this.props.nums} />
         )
     }
   }
 });
-
-
 
 
 ReactDOM.render(
